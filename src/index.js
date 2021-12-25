48b5fcf9e50c03130999761609053a119169cb9d
fatin-nadhirah <fatin.nadhirah@axa.com.my>
2021-12-26 04:44:20 +0800

    format index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Create from './pages/Create';
-import Navbar from './layouts/Navbar';
-import Sidebar from './layouts/Sidebar';
-import ListPage from './pages/List';
-import Update from './pages/Update';
-import Delete from './pages/Delete';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Create from "./pages/Create";
+import Navbar from "./layouts/Navbar";
+import Sidebar from "./layouts/Sidebar";
+import ListPage from "./pages/List";
+import Update from "./pages/Update";
+import Delete from "./pages/Delete";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar/>
+    <Navbar />
     {/* <Sidebar /> */}
-   <ListPage />
-   <Create />
-   <Update />
-   <Delete />
+    <ListPage />
+    <Create />
+    <Update />
+    <Delete />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
